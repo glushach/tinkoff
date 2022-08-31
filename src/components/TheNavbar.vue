b629ca92027c95450762dced548f912e2dcd3cd8
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-31 08:53:59 +0300

    feat: add boilerplate for login

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <h3>Online Bank</h3>
+    
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Request</router-link>
+      </li>
+      <li>
+        <router-link to="/">Help</router-link>
+      </li>
+      <li>
+        <router-link to="/">Messages</router-link>
+      </li>
+      <li>
+        <router-link to="/">Exit</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "TheNavbar"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <the-navbar></the-navbar>
+  <div class="container with-nav pt-1">
+    <div class="card">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TheNavbar from "../components/TheNavbar";
+  export default {
+    components: {TheNavbar}
+  }
+</script>
+
+<style scoped>
+
+</style>
